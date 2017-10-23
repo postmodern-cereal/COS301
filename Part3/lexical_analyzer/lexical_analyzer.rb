1c054aff733fdd7df50510539639fb12d7401413
postmodern-cereal <noah.ransom@maine.edu>
2017-10-23 15:49:52 -0400

    Completed analysis and began parsing

--- Part3/lexical_analyzer/lexical_analyzer.rb
+++ Part3/lexical_analyzer/lexical_analyzer.rb
@@ -5,7 +5,7 @@
 #declare the variables that you'll need later
 $char_class #character's class
 #will need to use $charclass.is_a?(Fixnum) to make sure that it's an integer
-$lexeme = Array.new() #we don't have to do anything else with this because ruby doesn't care
+$lexeme = Array.new(100) #we don't have to do anything else with this because ruby doesn't care
 $next_char #the next character to be read
 $lex_len 	#length of the lexeme
 $next_token	#number of the next token
@@ -27,6 +27,7 @@ $REL_OP  	 = 12
 $OR_OP   	 = 13
 $AND_OP		 = 14
 $REL_OP		 = 15	#for comparison and stuff
+$NEG_OP		 = 16	#exclamation point
 $LEFT_PAREN  = 20
 $RIGHT_PAREN = 21
 $BOOL_LIT 	 = 30	#true/false
@@ -66,8 +67,8 @@ end
 #add the next character to the lexeme
 def add_char()
 	if $lex_len <= 98
-		$lexeme[$lex_len++] = $next_char
-		$lexeme[$lex_len] = 0
+		$lexeme[$lex_len] = $next_char
+		$lex_len = $lex_len + 1
 	else
 		puts "Error: lexeme too long"
 	end
@@ -98,6 +99,16 @@ def lookup(character)
 	when ">", "<"
 		add_char
 		$next_token = $REL_OP
+	when "="		#if we have one =, the next character has to be an = to be legal
+		add_char
+		get_char	#get the next character, so that we can test it. If it's not what we want, that's bad
+		if $next_char == "="
+			add_char
+			$next_token = $REL_OP
+		else
+			#the string is not legal
+			abort("Error. Invalid relation operator. Analysis terminated.")
+		end
 	else
 		add_char
 		$next_token = $EOF
@@ -105,6 +116,7 @@ def lookup(character)
 end
 
 def lex()
+	$lexeme.clear
 	$lex_len = 0
 	get_non_blank
 	case $char_class
@@ -116,21 +128,20 @@ def lex()
 			get_char
 		end
 		#now that the word is fully read, we have to find ouot if it's an identifier or a boolean literal
-		if $lexeme.join == "true" || $lexeme == "false"
+		#to do this, we join the lexeme together
+		if $lexeme.join == "true" || $lexeme.join == "false"
 			$next_token = $BOOL_LIT
 		else
 			$next_token = $IDENT
 		end
 	when $DIGIT
-		#finish the lexeme
 		add_char
 		get_char
 		while $char_class == $DIGIT
 			add_char
 			get_char
 		end
-		puts "Error on lexeme " + $lexeme.join + ": identifiers may not begin with digits."
-		abort("Analysis terminated")
+		$next_token = $INT_LIT
 	when $UNKNOWN
 			lookup($next_char)
 			get_char
@@ -138,21 +149,62 @@ def lex()
 		$next_token = $EOF
 		$lexeme = ["E","O","F"]
 	end
-	puts "Next token is: " + $next_token + ", next lexeme is " + $lexeme.join
+	puts "Next token is: " + $next_token.to_s + ", next lexeme is " + $lexeme.join
 end
+
+def bool_expr()
+	#parse <and_term> { | <and_term>}
+	puts "enter <bool_expr>"
+	and_term
+	while $next_token == $OR_OP
+		lex()
+		term()
+	end
+	puts "exit <bool_expr>"
+end
+
+def and_term()
+	#parses and_term ::= bool_factor {& bool_factor}
+	puts "enter <and_term>"
+	bool_factor
+	while $next_token == $OR_OP
+		lex
+		bool_factor
+	end
+	puts "exit <and_term>"
+end
+
+def bool_factor()
+	puts "enter <bool_factor>"
+	if $next_token == $BOOL_LIT
+		lex
+	elsif $next_token == $NEG_OP
+		lex
+		bool_factor
+	elsif $next_token == $LEFT_PAREN
+		lex
+		bool_expr
+		if $next_token == $RIGHT_PAREN
+			lex
+		else
+			error
+		end
+	else
+		lex
+		relation_expr
+	end
+	puts "exit <bool_factor>"
+end
+
+
+
 
 
 ######################Main function analogue##############################
 #open input file in read mode
 $INPUT_FILE = File.open("input.txt", "r")
-getChar()
+get_char()
 until $next_token == $EOF do
 	lex()
 end
-
-
-
-END #everything in here is done at the very end and I can just ignore it
-{
-    $INPUT_FILE.close
-}
+$INPUT_FILE.close
